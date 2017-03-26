d54fc3bc2a591bdc901d0062cba28881692cc282
Gabe Montague <gmontague@college.harvard.edu>
2017-03-26 14:43:52 -0400

    Media hosting

--- tracktalk/urls.py
+++ tracktalk/urls.py
@@ -3,9 +3,14 @@ from __future__ import unicode_literals
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.urls')),
     url(r'^', include('pybb.urls', namespace='pybb')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
