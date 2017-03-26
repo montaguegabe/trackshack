dcb7bf420b985cd145b0c0bde16c865a19545764
Gabe Montague <gmontague@college.harvard.edu>
2017-03-26 12:55:01 -0400

    Initial commit

---
+++ tracktalk/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('registration.urls')),
+    url(r'^', include('pybb.urls', namespace='pybb')),
+]
